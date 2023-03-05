639cb7ab36e2eab260922e642664df6b3859a91c
Oscar <oscar0694nogales@gmail.com>
2023-03-05 17:19:07 -0500

    clases con vue

---
+++ main2.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+    el: '#app',
+    data:{
+        moviles:[
+            {nombre: 'Samsung', precio: 128},
+            {nombre: 'Xiami', precio: 145},
+            {nombre: 'Nokia', precio: 200},
+            {nombre: 'Apple', precio: 500}
+
+        ]
+    } 
+
+
+})
